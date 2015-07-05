cc7736130bfd4eec86aec0631ecb08f71cc0f340
bongster <bongster88@gmail.com>
2015-07-05 15:24:29 +0900

    [#171] dbconfig file setting

--- soma3/fabfile.py
+++ soma3/fabfile.py
@@ -1,9 +1,31 @@
 from __future__ import with_statement
 from fabric.api import local, settings, run, cd, env, prefix
 from fabric.contrib.console import confirm
+from fabric.contrib.files import exists
+from fabric.operations import prompt, put
 
 env.directory = '/home/URQA-Server'
 env.activate = 'source ~/.virtualenvs/urqa/bin/activate'
+
+
+def config():
+    env.config_file = prompt(
+        'where is global config file: ',
+        default='~/Documents/config.cfg')
+    env.google_file = prompt(
+        'where is google config file: ',
+        default='~/Documents/google_client_secrets.json')
+    env.db_config_file = prompt(
+        'where is db config file: ',
+        default='~/Documents/dbconfig.cfg')
+
+    with prefix(env.activate):
+        with cd(env.directory):
+            if not exists('/data/etc/urqa.io/'):
+                run('mkdir -p /data/etc/urqa.io')
+            put(env.config_file, '/data/etc/urqa.io/config.cfg')
+            put(env.google_file, '/data/etc/urqa.io/google_client_secrets.json')
+            put(env.dbconfig, '/data/etc/urqa.io/dbconfig.cfg')
 
 
 def test():
@@ -21,6 +43,7 @@ def deploy():
 def setting():
     pip()
     git()
+    config()
     code_dir = '~/.virtualenvs/urqa'
     with settings(warn_only=True):
         if run('test -d %s' % code_dir).failed:
